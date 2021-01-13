ea86367e76fc67e14eb6baf19605980381fd9230
darrickpang <dkypang@yahoo.com>
2021-01-13 14:36:31 -0800

    user stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_secure_password :recovery_password, validations: false
+
+    validates :name, uniqueness: true
+    validates :name presence: true
 end
