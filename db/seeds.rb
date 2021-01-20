597f497ac23306784ffa77f42214430e4b8e10d0
darrickpang <dkypang@yahoo.com>
2021-01-19 16:20:29 -0800

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,4 @@ ChemicalUser.destroy_all
 User.create(name: 'Darrick Pang', password: '123')
 
 ChemicalUser.create(user_id: 1, level: 4, date: "2021/01/17", time: "3:25 PM")
+ChemicalUser.create(user_id: 1, level: 5, date: "2021/01/19", time: "4:19 PM")
